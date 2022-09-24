a54a5f890f99ee1cf7dd04ad7d9d71dbd6abbf8d
rayhanputtra <114299304+rayhanputtra@users.noreply.github.com>
2022-09-24 21:53:33 +0700

    config

---
+++ config/response.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+commonError:{
+    error: true,
+    msg : 'Terjadi Kesalahan Pada Server'
+},
+commonErrorMsg: (msg) => {
+    return {
+        error: true,
+        msg : msg
+    }
+},
+
+commonSuccess: {
+    error: false,
+    msg : 'Berhasil Membuat Permintaan'
+},
+commonSuccessMsg: (msg) => {
+    return{
+        error:false,
+        msg : msg
+    }
+},
+commonResult: (data) => {
+    return {
+        error : false,
+        msg : 'Berhasil Memuat Data',
+        data : data
+    }
+}
+
+};
